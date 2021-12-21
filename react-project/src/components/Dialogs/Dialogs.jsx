0f7e587d936c89532cb37be16943205e22f4d165
Alexandr Kaplunov <kaplunovdev@gmail.com>
2021-12-21 18:04:02 +0300

    Added Redirect(Navigate to={})

--- react-project/src/components/Dialogs/Dialogs.jsx
+++ react-project/src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogsItem";
 import Message from "./Message/Message";
+import {Navigate} from "react-router-dom";
 
 const Dialogs = (props) => {
     let state = props.dialogsPage
@@ -19,6 +20,8 @@ const Dialogs = (props) => {
         let body = e.target.value
         props.updateNewMessageBody(body)
     }
+
+    if(!props.isAuth)return <Navigate to='/login'/>
     return (
         <div>
             <div className={style.chatMain}>
--- react-project/src/components/Profile/ProfileContainer.jsx
+++ react-project/src/components/Profile/ProfileContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Profile from "./Profile";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../redux/profile-reducer";
-import {useParams} from "react-router-dom"
+import {Navigate, useParams} from "react-router-dom"
 
 const withRouter = WrappedComponent => props => {
     const params = useParams();
@@ -14,21 +14,23 @@ const withRouter = WrappedComponent => props => {
     );
 };
 
-class ProfileContainer extends React.Component{
+class ProfileContainer extends React.Component {
 
     componentDidMount() {
         let userId = this.props.params.userId;
-        if (!userId){
-            userId=2;
+        if (!userId) {
+            userId = 2;
         }
         this.props.getUserProfile(userId)
 
     }
 
     render() {
+        if (!this.props.isAuth) return <Navigate to='/login'/>
+
         return (
             <div>
-                <Profile {...this.props} profile={this.props.profile} />
+                <Profile {...this.props} profile={this.props.profile}/>
             </div>
         )
     }
@@ -36,7 +38,8 @@ class ProfileContainer extends React.Component{
 };
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    isAuth: state.auth.isAuth
 })
 let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
